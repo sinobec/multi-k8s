d62e2edb60463966c35ba9004117daf78bbe5bd9
Alex Gao <pengbuddy@hotmail.com>
2020-07-20 15:55:45 -0400

    test

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ class App extends React.Component {
                     <header className='App-header'>
                         <img src={logo} className='App-logo' alt='logo' />
                         <p>
-                            Edit <code>src/App.js</code> and save to reload.
+                            FIB CACULATOR
                         </p>
                         <a
                             className='App-link'
